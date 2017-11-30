96f08a4b9cc0b6f0a8b726ea3ba26d4e970d8d8e
dimavdovi22 <32964603+dimavdovi22@users.noreply.github.com>
2017-11-30 12:29:05 +0200

    Add files via upload

---
+++ Untitled66.m
@@ -0,0 +1,23 @@
+%Завдання 1.3
+p1=[2 6 4 7 9 1]
+p2=[4 2 4 8 3 1]
+tf(p1,p2)
+tf(p2,p1)
+clsys1=feedback(tf(p1,p2),tf(p2,p1))
+bl3=parallel(tf(p1,p2),tf(p2,p1))
+clsys1=feedback(tf(p1,p2),tf(p2,p1))
+%Завдання 1.4
+disp('поліном')
+P=[1 0.3 4.3 0.4]
+disp('корені поліномів')
+R=roots(P)
+disp('перехід від коренів до поліному')
+P=poly(R)
+%Завдання 1.5
+CL1=zpk(clsys1)
+figure(4)
+pzmap(clsys1),grid on
+clsys4=feedback(tf(p2,p1),tf(p1,p2),1)
+
+
+
